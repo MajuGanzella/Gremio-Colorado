6d12d02b955bc2e935b73ba37837a2d5726dcf0f
MajuGanzella <maju.ganzella@gmail.com>
2024-10-23 09:40:03 -0300

    Add files via upload

---
+++ ArquivoFinalGremio/_javascript/central.js
@@ -0,0 +1,23 @@
+function validarFormulario(){
+    const nomeRegex = /^[a-zA-Z\s]+$/;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+   
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+   
+    
+    if(!nomeRegex.test(nome)){
+        alert("Nome Inválido.Use apenas letras e espaços.");
+        return false;
+    }
+
+
+
+    if(!emailRegex.test(email)){
+        alert("E-mail inválido. Insira um e-mail válido")
+        return false;
+    }
+   
+ 
+    return true;
+}
